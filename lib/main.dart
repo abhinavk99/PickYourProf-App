a3600f438886640de424af7a1afdb54f0db34a99
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2018-07-23 21:18:01 -0700

    Added UI for form

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,18 @@ class PickYourProf extends StatelessWidget {
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
         // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: const MaterialColor(0xFFbf5700, const <int, Color> {
+          50:  const Color(0xFFbf5700),
+          100: const Color(0xFFbf5700),
+          200: const Color(0xFFbf5700),
+          300: const Color(0xFFbf5700),
+          400: const Color(0xFFbf5700),
+          500: const Color(0xFFbf5700),
+          600: const Color(0xFFbf5700),
+          700: const Color(0xFFbf5700),
+          800: const Color(0xFFbf5700),
+          900: const Color(0xFFbf5700),
+        }),
       ),
       home: new HomePage(title: 'PickYourProf'),
     );
@@ -57,11 +68,122 @@ class _HomePageState extends State<HomePage> {
         // the App.build method, and use it to set our appbar title.
         title: new Text(widget.title),
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Center(
-          child: new Text('Find the best professor.'),
+      body: new Container(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'Find the best professor.',
+              style: new TextStyle(
+                fontSize: 24.0,
+              ),
+            ),
+            new Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: new _CourseForm(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CourseForm extends StatefulWidget {
+  @override
+  _CourseFormState createState() {
+    return new _CourseFormState();
+  }
+}
+
+class _CourseData {
+  String department = '';
+  String courseNumber = '';
+}
+
+class _CourseFormState extends State<_CourseForm> {
+  // Create a global key that will uniquely identify the Form widget and allow
+  // us to validate the form
+  //
+  // Note: This is a `GlobalKey<FormState>`, not a GlobalKey<_CourseFormState>!
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  _CourseData _data = new _CourseData();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey we created above
+    return new Container(
+      alignment: Alignment.center,
+      child: new Form(
+        key: this._formKey,
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Container(
+              width: 350.0,
+              child: new TextFormField(
+                decoration: new InputDecoration(
+                  labelText: 'Department',
+                  hintText: 'Ex: CS'
+                ),
+                textAlign: TextAlign.center,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'Please enter a department';
+                  }
+                  return null;
+                },
+                onSaved: (String value) {
+                  this._data.department = value;
+                },
+              ),
+            ),
+            new Container(
+              width: 350.0,
+              child: new TextFormField(
+                decoration: new InputDecoration(
+                  labelText: 'Course Number',
+                  hintText: 'Ex: 314'
+                ),
+                textAlign: TextAlign.center,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'Please enter a course number';
+                  }
+                  return null;
+                },
+                onSaved: (String value) {
+                  this._data.courseNumber = value;
+                },
+              ),
+            ),
+            new Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30.0),
+              child: new RaisedButton(
+                onPressed: () {
+                  if (this._formKey.currentState.validate()) {
+                    _formKey.currentState.save();
+                    Scaffold
+                      .of(context)
+                      .showSnackBar(
+                      new SnackBar(
+                        content: Text(
+                          'Getting information for ${_data.department} ${_data.courseNumber}'
+                        )
+                      )
+                    );
+                  }
+                },
+                child: new Text(
+                  'Submit',
+                  style: new TextStyle(
+                      color: Colors.white
+                  ),
+                ),
+                color: Theme.of(context).accentColor,
+              ),
+            ),
+          ],
         ),
       ),
     );
