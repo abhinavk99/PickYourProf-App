2e044aaad4ed670dcac72dd984c1d566188e14ed
SamarthDesai01 <37949934+SamarthDesai01@users.noreply.github.com>
2018-07-24 00:35:46 -0500

    Move form into elevated card

--- lib/widgets/course_form.dart
+++ lib/widgets/course_form.dart
@@ -21,62 +21,67 @@ class CourseFormState extends State<CourseForm> {
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey we created above
-    return new Container(
-      alignment: Alignment.center,
-      child: new Form(
-        key: this._formKey,
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Container(
-              width: 350.0,
-              child: new TextFormField(
-                decoration: new InputDecoration(labelText: 'Department', hintText: 'Ex: CS'),
-                textAlign: TextAlign.center,
-                validator: (String value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a department';
-                  }
-                  return null;
-                },
-                onSaved: (String value) {
-                  this._data.department = value;
-                },
-              ),
-            ),
-            new Container(
-              width: 350.0,
-              child: new TextFormField(
-                decoration: new InputDecoration(labelText: 'Course Number', hintText: 'Ex: 314'),
-                textAlign: TextAlign.center,
-                validator: (String value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a course number';
-                  }
-                  return null;
-                },
-                onSaved: (String value) {
-                  this._data.courseNumber = value;
-                },
-              ),
-            ),
-            new Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30.0),
-              child: new RaisedButton(
-                onPressed: () {
-                  if (this._formKey.currentState.validate()) {
-                    _formKey.currentState.save();
-                    Scaffold.of(context).showSnackBar(new SnackBar(content: Text('Getting information for ${_data.department} ${_data.courseNumber}')));
-                  }
-                },
-                child: new Text(
-                  'Submit',
-                  style: new TextStyle(color: Colors.white),
+    return new Center(
+      child: new Card(
+        elevation: 4.0,
+        child: new Padding(
+          padding: EdgeInsets.all(12.0),
+          child: new Form(
+            key: this._formKey,
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Container(
+                  width: 350.0,
+                  child: new TextFormField(
+                    decoration: new InputDecoration(labelText: 'Department', hintText: 'Ex: CS'),
+                    textAlign: TextAlign.center,
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Please enter a department';
+                      }
+                      return null;
+                    },
+                    onSaved: (String value) {
+                      this._data.department = value;
+                    },
+                  ),
                 ),
-                color: Theme.of(context).accentColor,
-              ),
+                new Container(
+                  width: 350.0,
+                  child: new TextFormField(
+                    decoration: new InputDecoration(labelText: 'Course Number', hintText: 'Ex: 314'),
+                    textAlign: TextAlign.center,
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Please enter a course number';
+                      }
+                      return null;
+                    },
+                    onSaved: (String value) {
+                      this._data.courseNumber = value;
+                    },
+                  ),
+                ),
+                new Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 30.0),
+                  child: new RaisedButton(
+                    onPressed: () {
+                      if (this._formKey.currentState.validate()) {
+                        _formKey.currentState.save();
+                        Scaffold.of(context).showSnackBar(new SnackBar(content: Text('Getting information for ${_data.department} ${_data.courseNumber}')));
+                      }
+                    },
+                    child: new Text(
+                      'Submit',
+                      style: new TextStyle(color: Colors.white),
+                    ),
+                    color: Theme.of(context).accentColor,
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
