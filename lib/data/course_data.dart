6b8d3ae89626bf85e9e9c209c884848a51ca9cb8
SamarthDesai01 <37949934+SamarthDesai01@users.noreply.github.com>
2018-07-27 15:26:48 -0500

    Complete CourseData class
    Added default constructor, getters, and setters

--- lib/data/course_data.dart
+++ lib/data/course_data.dart
@@ -1,4 +1,18 @@
 class CourseData {
-  String department = '';
-  String courseNumber = '';
+  String _department = '';
+  String _courseNumber = '';
+
+  CourseData(this._department, this._courseNumber);
+
+  String get department => _department;
+
+  String get courseNumber => _courseNumber;
+
+  set courseNumber(String value) {
+    _courseNumber = value;
+  }
+
+  set department(String value) {
+    _department = value;
+  }
 }
