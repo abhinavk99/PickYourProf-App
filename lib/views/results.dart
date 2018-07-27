1f1fc7a304835de35f3243f7328173cff6620d67
SamarthDesai01 <37949934+SamarthDesai01@users.noreply.github.com>
2018-07-27 15:28:10 -0500

    Create empty results page

---
+++ lib/views/results.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+class ResultsScreen extends StatefulWidget {
+  @override
+  _ResultsScreenState createState() => _ResultsScreenState();
+}
+
+class _ResultsScreenState extends State<ResultsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
